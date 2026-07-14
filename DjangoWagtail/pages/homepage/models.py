1d36c68616a4b9a3996506492710a7a7674e784d
Mees v A <meessievanaalten@gmail.com>
2026-07-14 10:34:12 +0200

    Added a simple block to a page. no stysling yet

--- DjangoWagtail/pages/homepage/models.py
+++ DjangoWagtail/pages/homepage/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+from DjangoWagtail.blocks.promoted_block.models import PromotedBlock
 
 
-# Create your models here:
 class HomePage(Page):
     body = models.TextField(blank=True, max_length=500)
     quote = models.TextField(blank=True, max_length=100)
+
+    StreamField = StreamField(
+        [
+            ("promoted", PromotedBlock()),
+        ],
+        use_json_field=True,
+        default=[],
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel("quote"),
         FieldPanel("body"),
+        FieldPanel("StreamField"),
     ]
